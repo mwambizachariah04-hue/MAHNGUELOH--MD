5dd22409b3bf0fe3a40d919a338cbdffe4d47003
Mwambi <mwambizachariah04@gmail.com>
2026-03-21 09:19:36 +0300

    Create config.js file for WhatsApp bot configuration

---
+++ config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+    WHATSAPP_API_KEY: process.env.WHATSAPP_API_KEY,
+    WHATSAPP_API_URL: process.env.WHATSAPP_API_URL,
+    WHATSAPP_PHONE_NUMBER: process.env.WHATSAPP_PHONE_NUMBER,
+};
