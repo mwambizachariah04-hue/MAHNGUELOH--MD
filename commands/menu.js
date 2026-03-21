799e0830cc9da3261405824aad3b2e522cb545dc
Mwambi <mwambizachariah04@gmail.com>
2026-03-21 09:43:00 +0300

    Create menu.js file with comprehensive menu command listing

---
+++ commands/menu.js
@@ -0,0 +1,42 @@
+// menu.js
+
+const commands = {
+    AI: [    // List of AI commands
+        'AI Command 1',
+        'AI Command 2',
+        'AI Command 3',
+    ],
+    audio: [ // List of Audio commands
+        'Audio Command 1',
+        'Audio Command 2',
+    ],
+    download: [ // List of Download commands
+        'Download Command 1',
+    ],
+    fun: [ // List of Fun commands
+        'Fun Command 1',
+        'Fun Command 2',
+    ],
+    group: [ // List of Group commands
+        'Group Command 1',
+    ],
+    image: [ // List of Image commands
+        'Image Command 1',
+        'Image Command 2',
+    ],
+    search: [ // List of Search commands
+        'Search Command 1',
+        'Search Command 2',
+    ],
+    tools: [ // List of Tools commands
+        'Tool Command 1',
+    ],
+    settings: [ // List of Settings commands
+        'Settings Command 1',
+    ],
+    owner: [ // List of Owner commands
+        'Owner Command 1',
+    ]
+};
+
+module.exports = commands;
