c0433a3b4317560a21fcb5d04d555af8611dc108
Mwambi <mwambizachariah04@gmail.com>
2026-03-21 10:39:47 +0300

    Updated index.js with comprehensive command list.

--- index.js
+++ index.js
@@ -1,28 +1,76 @@
-// Commands
-const commands = [
-    'kick',
-    'promote',
-    'demote',
-    'link',
-    'resetlink',
-    'tagall',
-    'tagadmin',
-    'hidetag',
-    'open',
-    'close',
-    'setgroupname',
-    'setdesc',
-    'setppgroup',
-    'welcome',
-    'antibot',
-    'antilink',
-    'antitag',
-    'antigroupmention',
-    'approve',
-    'reject',
-    'poll',
-    'totalmembers',
-    'listactive'
-];
+# Command List
 
-module.exports = commands;
+## AI Menu
+- blackbox
+- code
+- dalle
+- deepseek
+- doppleai
+- gemini
+- generate
+- gpt
+- programming
+- recipe
+- story
+- summarize
+- teach
+- translate2
+
+## Audio Menu
+- bass
+- blown
+- deep
+- earrape
+- reverse
+- robot
+- tomp3
+- toptt
+- volaudio
+
+## Download Menu
+- apk
+- download
+- facebook
+- gdrive
+- gitclone
+- image
+- instagram
+- itunes
+- mediafire
+- pin
+- savestatus
+- song
+- song2
+- telesticker
+- tiktok
+- tiktokaudio
+- twitter
+- video
+- videodoc
+- xvideo
+
+## Ephoto360 Effects
+- 1917style
+- writetext
+
+## Fun Menu
+
+## Games Menu
+
+## Extended Group Management
+
+## Owner Menu
+
+## Religion Menu
+
+## Search Menu
+
+## Settings Menu
+
+## Support Menu
+
+## Tools Menu
+
+## Translate Menu
+
+## Video Menu
