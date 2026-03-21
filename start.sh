a232fdb66bbb194d17ea23d2448d013ce655e5d7
Mwambi <mwambizachariah04@gmail.com>
2026-03-21 10:46:18 +0300

    Create start.sh for Pterodactyl Panel deployment

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Start script for Pterodactyl Panel Deployment
+
+# Log the current date and time
+echo "Starting Pterodactyl panel deployment at $(date)"
+
+# Commands to start the deployment
+# Add your specific commands for starting the Pterodactyl panel here
