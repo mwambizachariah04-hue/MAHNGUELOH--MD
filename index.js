2d09e956467b206c8ac810bd7e60316db0d14b98
Mwambi <mwambizachariah04@gmail.com>
2026-03-21 10:37:21 +0300

    Update index.js to add new commands

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-// Additional commands added
-
-const commands = {
-    antibug: 'description for antibug command',
-    ban: 'description for ban command',
-    unban: 'description for unban command',
-    antivn: 'description for antivn command',
-    antikick: 'description for antikick command',
-    admin: 'description for admin command'
-};
+// Commands
+const commands = [
+    'kick',
+    'promote',
+    'demote',
+    'link',
+    'resetlink',
+    'tagall',
+    'tagadmin',
+    'hidetag',
+    'open',
+    'close',
+    'setgroupname',
+    'setdesc',
+    'setppgroup',
+    'welcome',
+    'antibot',
+    'antilink',
+    'antitag',
+    'antigroupmention',
+    'approve',
+    'reject',
+    'poll',
+    'totalmembers',
+    'listactive'
+];
 
 module.exports = commands;
