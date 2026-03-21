7e4e7e4abbe6b10ef5d38a9443c4f04196821a8e
Mwambi <mwambizachariah04@gmail.com>
2026-03-21 10:33:12 +0300

    Update index.js to add new commands

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
-const { WAConnection, MessageType } = require('@adiwajshing/baileys');
-const fs = require('fs');
+// Additional commands added
 
-const ownerNumber = '254732223354';
-const botName = 'MAHNGUELOH-MD';
-const conn = new WAConnection();
-
-async function connect() {
-    conn.on('qr', qr => {
-        console.log('SCAN THIS QR CODE:', qr);
-    });
-
-    conn.on('open', () => {
-        console.log('Bot is connected');
-    });
-
-    await conn.connect();
-    console.log('Connected as:', conn.user.jid);
-}
-
-const commandHandlers = {
-    ping: (msg) => `Bot is online!`,
-    hello: (msg) => `Hello, ${msg.sender}!`,
-    help: (msg) => `Available commands: ping, hello, help, info, owner`,
-    info: (msg) => `This bot is named ${botName}.`,
-    owner: (msg) => `Owner: ${ownerNumber}`
+const commands = {
+    antibug: 'description for antibug command',
+    ban: 'description for ban command',
+    unban: 'description for unban command',
+    antivn: 'description for antivn command',
+    antikick: 'description for antikick command',
+    admin: 'description for admin command'
 };
 
-conn.on('chat-update', async chatUpdate => {
-    if (!chatUpdate.hasNewMessage) return;
-    const message = chatUpdate.messages.all()[0];
-    const messageContent = message.message.conversation;
-    const command = messageContent.split(' ')[0];
-
-    if (command in commandHandlers) {
-        const response = commandHandlers[command](message);
-        await conn.sendMessage(message.key.remoteJid, response, MessageType.text);
-    }
-});
-
-connect();
+module.exports = commands;
